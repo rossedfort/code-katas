0bb3b130f84a11ad6777487d0bdf885e94bcf6cd
Ross Edfort <ross.edfort@wazeedigital.com>
2018-10-03 08:52:21 -0600

    square every digit

---
+++ square-every-digit.js
@@ -0,0 +1,7 @@
+// http://www.codewars.com/kata/square-every-digit
+
+function squareDigits(num) {
+  return Number(num.toString().split('').map(n => n ** 2).join(''));
+}
+
+console.log(squareDigits(9119), 811181);
